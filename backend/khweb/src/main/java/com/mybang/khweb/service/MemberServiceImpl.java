4e21432fe67f349cefadcacbecaf7fb0774b73fa
jaqkqh15 <jaqkqh15@naver.com>
2021-10-14 18:45:24 +0900

    Create Member Default File

---
+++ backend/khweb/src/main/java/com/mybang/khweb/service/MemberServiceImpl.java
@@ -0,0 +1,24 @@
+package com.mybang.khweb.service;
+
+import com.mybang.khweb.repository.MemberAuthRepository;
+import com.mybang.khweb.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@Lazy
+@Slf4j
+public class MemberServiceImpl implements MemberService{
+
+    @Autowired
+    private MemberRepository repository;
+
+    @Autowired
+    private MemberAuthRepository authRepository;
+
+    @Autowired
+    private PasswordEncoder encoder;
+}
