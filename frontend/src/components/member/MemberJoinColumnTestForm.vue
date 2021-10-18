88977650acf18b95cbc2214981278b4781bdd678
rudghxodid <rudghxodid@naver.com>
2021-10-18 20:33:47 +0900

    회원가입(front)

---
+++ frontend/src/components/member/MemberJoinColumnTestForm.vue
@@ -0,0 +1,120 @@
+<template>
+    <v-container>
+        <form @submit.prevent="onSubmit">
+            <h3>회원 가입</h3>
+            <div class="row">
+                <v-radio-group v-model="radioGroup" row>
+                    <v-radio v-for="kinds in kindsOfMember" :key="kinds" :label="`${kinds}`"> 
+                    </v-radio>
+                </v-radio-group>
+                
+            </div>
+           <div class="mx-3"> <v-icon color="black" size="30px">person</v-icon>
+                 ID
+                <div class="mx-1">
+                 <v-text-field placeholder="ID" v-model="userId" required >
+                </v-text-field>
+                 </div>
+                  </div> 
+
+
+            <div class="mx-3"> 
+                <v-icon color="black" size="30px">lock</v-icon>
+                    Password
+                    <div class="mx-1">
+                     <v-text-field placeholder="Password" type="password" v-model="password" required 
+                     ></v-text-field>
+                     </div> 
+                     </div>
+
+            <div class="mx-3"> 
+                <v-icon color="black" size="30px">mail</v-icon>
+                 E-mail
+                <div class="mx-1">
+                    <v-text-field placeholder="name" v-model="email" required 
+                        ></v-text-field>
+                        </div> 
+                        </div> 
+
+
+
+            <div class="mx-3"> 
+                <v-icon color="black" size="30px">label</v-icon>
+                 Name
+                <div class="mx-1">
+                    <v-text-field placeholder="name" v-model="name" required 
+                        ></v-text-field>
+                        </div> 
+                        </div> 
+
+            <div class="mx-3"> 
+                <v-icon color="black" size="30px">hourglass_empty</v-icon>
+                 Age
+                <div class="mx-1">
+                    <v-text-field placeholder="name" v-model="age" required 
+                        ></v-text-field>
+                        </div> 
+                        </div>
+
+            <div class="mx-3"> 
+                <v-icon color="black" size="30px">wc</v-icon>
+                 Sex
+                <div class="mx-1">
+                    <v-text-field placeholder="name" v-model="sex" required 
+                        ></v-text-field>
+                        </div> 
+                        </div>  
+
+            <div class="mx-3"> 
+                <v-icon color="black" size="30px">phone</v-icon>
+                 Phone
+                <div class="mx-1">
+                    <v-text-field placeholder="name" v-model="phone" required 
+                        ></v-text-field>
+                        </div> 
+                        </div> 
+                 
+
+            <div>
+                <button type="submit">등록</button>
+                <router-link :to="{ name: 'Home' }">
+                    취소
+                </router-link>
+            </div>
+        </form>
+    </v-container>
+</template>
+
+<script>
+
+export default {
+    name: 'MemberJoinColumnTestForm',
+    data () {
+        return {
+            radioGroup: 1,
+            kindsOfMember: [
+                '개인',
+                '사업자'
+            ],
+            userId: '',
+            password: '',
+            email: '',
+            name: '',
+            age: '',
+            sex: '',
+            phone: ''
+            
+        }
+    },
+    methods: {
+        onSubmit () {
+            const { userId, password, email, name, age, sex, phone, radioGroup } = this
+            const auth = radioGroup == 0 ? '개인' : '사업자'
+            this.$emit('submit', { userId, name,password, email, name, age, sex, phone, auth })
+        },
+        
+        
+       
+    }
+}
+</script>
---
+++ frontend/src/views/MemberRegisterPage.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <div align="center">
+            <h2>회원 가입</h2>
+        </div>
+        <member-join-column-test-form @submit="onSubmit"/>
+        
+        <v-spacer></v-spacer>
+       
+            
+     
+    </div>
+</template>
+
+<script>
+import MemberJoinColumnTestForm from '@/components/member/MemberJoinColumnTestForm.vue'
+import axios from 'axios'
+export default {
+    name: 'MemberRegisterPage',
+    components: {
+        MemberJoinColumnTestForm
+    },
+    methods: {
+        onSubmit (payload) {
+            const { userId, password, email, name, age, sex, phone, auth } = payload
+            axios.post('http://localhost:7777/jpamember/register', {
+                        userId, name, password,email, name, age, sex, phone, auth
+                    })
+                    .then(res => {
+                        alert('회원가입이 완료되었습니다' + res)
+                        
+                        this.$router.push({
+                            name: 'Home'
+                            
+                        })
+                        
+                    })
+                    .catch(res => {
+                        alert(res.response.data.message)
+                    })
+        }
+    }
+}
+</script>
