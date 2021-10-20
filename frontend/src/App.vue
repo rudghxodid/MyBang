dcd0256fa2e680e37c2aec6f7ce9f37a0f20885f
jaqkqh15 <jaqkqh15@naver.com>
2021-10-20 19:14:08 +0900

    [황정아] 메인페이지 적용

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <v-app>
     <v-main>
+      <header></header>
       <router-view/>
+      <footer></footer>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import Footer from '@/layouts/Footer'
+import Header from '@/layouts/Header'
+
 
 export default {
   name: 'App',
- 
-
+  components: {
+    Header,
+    Footer
+    
+  },
+    
   data: () => ({
     
   }),
