d23c441516a8b0c6463540145dbb6a0e8a43b682
jaqkqh15 <jaqkqh15@naver.com>
2021-10-20 19:45:38 +0900

    .

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,17 @@
 <template>
   <v-app>
     <v-main>
-      <header></header>
       <router-view/>
-      <footer></footer>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import Footer from '@/layouts/Footer'
-import Header from '@/layouts/Header'
-
 
 export default {
   name: 'App',
-  components: {
-    Header,
-    Footer
-    
-  },
-    
+ 
+
   data: () => ({
     
   }),
