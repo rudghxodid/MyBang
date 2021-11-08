bbbb78b56e39ddf06685ec7d7dc37d623fc846fe
jaqkqh15 <jaqkqh15@naver.com>
2021-11-08 19:09:49 +0900

    [황정아] 지도 - 서울(구) 데이터 레이어

--- frontend/src/components/house/Map.vue
+++ frontend/src/components/house/Map.vue
@@ -5,17 +5,18 @@
     <naver-map
       ref="maps"
       :center="{ lat: center.lat, lng: center.lng }"
-      :zoom="12"
+      :zoom="zoomLevel"
       style="width: 70%; height: 100%; float: left;"
       @idle="idle">
 
-      <naver-map-marker-cluster :options="cluster.options">
-        <naver-map-marker v-for="list in villaList" :key="list.index"
-          :options="{ position: { lat: list.lat, lng: list.lng }}"
-          @click="viewInfo(list)"/>
-      </naver-map-marker-cluster>
-
-
+      <div v-if="zoomLevel >= 14">
+        <naver-map-marker-cluster :options="cluster.options">
+          <naver-map-marker v-for="list in villaList" :key="list.index"
+            :options="{ position: { lat: list.lat, lng: list.lng } }"
+            @click="viewInfo(list)"/>
+        </naver-map-marker-cluster>
+      </div>
+     
       <div v-if="zoomLevel >= 15">
         <naver-map-marker
           v-for="list in stationList" :key="list.index"
@@ -40,6 +41,7 @@
 import Info from '@/components/house/Info'
 import Search from '@/components/house/Search'
 import axios from 'axios'
+import seoulGu from '@/assets/data/seoul_gu'
 
 export default {
   components: {
@@ -65,16 +67,32 @@ export default {
         }
       },
       houseInfo: null,
-      zoomLevel: null,
-      icon: { content: `<div class="marker-html" v-html=list.deposit"></div>` }
+      zoomLevel: 11,
+      infoWindow: null
+    }
+  },
+  watch: {
+    zoomLevel: function () {
+      if (this.zoomLevel <= 12) {
+        if (this.$refs.maps.map.data._features.length == 0) {
+          this.startDataLayer(seoulGu)
+        }
+      } else {
+        this.removeDataLayer(seoulGu)
+      }
     }
   },
   mounted () {
+    setTimeout(() => {
+      this.startDataLayer(seoulGu)
+    }, 300)
+
     axios.get('http://localhost:7777/villa/lists').then(res => {
       this.villaList = res.data
       console.log(this.villaList)
       this.houseInfo = this.villaList[0]
     })
+
     axios.get('http://localhost:7777/station/lists').then(res => {
       this.stationList = res.data
       console.log(this.stationList)
@@ -108,14 +126,60 @@ export default {
     },
     selectStation (lat, lng) {
       this.showCircle(lat,lng)
-    }
+    },
+    startDataLayer(geojson) {
+      this.$refs.maps.map.data.addGeoJson(geojson)
+
+      this.$refs.maps.map.data.setStyle(function() {
+        return {
+          fillColor: 'white',
+          fillOpacity: 0.5,
+          strokeColor: 'blue',
+          strokeWeight: 2,
+          icon: null
+        }
+      })
+
+      this.$refs.maps.map.data.addListener('click', (e) => {
+        const coords = new window.naver.maps.LatLng(e.feature.getProperty('lat'), e.feature.getProperty('lng'))
+        this.$refs.maps.map.setCenter(coords)
+        this.$refs.maps.map.setZoom(14)
+      })
+
+      this.infoWindow = new window.naver.maps.InfoWindow({
+        disableAnchor: true,
+        backgroundColor: 'transparent',
+        borderWidth: 0
+      })
+
+      this.$refs.maps.map.data.addListener('mouseover', (e) => {
+        this.$refs.maps.map.data.overrideStyle(e.feature, {
+          fillColor: 'blue',
+          fillOpacity: 0.5,
+        })
+
+        let infoContent = `<div class="tooltip">${e.feature.getProperty('SGG_NM')}</div>`
+
+        this.infoWindow.setContent(infoContent)
+
+        this.infoWindow.open(this.$refs.maps.map, e.coord)
+      })
+      this.$refs.maps.map.data.addListener('mouseout', () => {
+        this.$refs.maps.map.data.revertStyle()
+        this.infoWindow.close()
+      })
+    },
+    removeDataLayer (geojson) {
+      this.$refs.maps.map.data.removeGeoJson(geojson)
+      this.infoWindow.close()
+    },
   }
 }
 </script>
 
 
 <style lang="scss">
-.marker-html {
+  .marker-html {
     position: relative;
     background: white;
     line-height: 30px;
@@ -166,5 +230,16 @@ export default {
   .lv5 {
     border-color:#04009A;
     background-color:#04009A;
+  }
+  .tooltip {
+    background: white;
+    line-height: 30px;
+    text-align: center;
+    font-weight: bold;
+    border-radius: 15px;
+    transition: 0.5s;
+    padding: 0 8px;
+    box-shadow: rgba(0, 0, 0, 0.08) 0px 0px 0px 1px,
+      rgba(0, 0, 0, 0.18) 0px 1px 2px;
   }
 </style>
